d96301f1e3473227c499e97dc593d9de0275ab16
unknown <SmithaRai.com>
2022-06-09 21:14:00 +0530

    Added : to display user input name

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component {
   url = 'https://www.bridgelabz.com/'
   constructor(){
-    super();
-    this.state={
-      title:'hello from Bridgelabz'
+    super()
+    this.State = {
+      userName :' '
     }
   } 
 
   //onClick function
   onClick = ($event) => {
     console.log("save button is clicked!",$event);
-    window.open(this.url ,"blank");
+    window.open( this.url ,"_blank");
+  }
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    //set the title using setState method
+    this.setState({ userName : event.target.value } )
   }
   render() {
     return (
+      <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1> hello {this.State.userName }from Bridgelabz </h1>
         <img src={logo} onClick = {this.onClick}
          alt = "The Bridgelabz logo: a bridge to employment through lab works"/>
       </div>
+      <div>
+        <input onChange = {this.onNameChange} />
+      </div>
+      </>
     );
   }
 }
-
-
 export default App;
