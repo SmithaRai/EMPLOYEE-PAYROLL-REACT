62f769d86895b30dd7c80f146a4e926dc353d54b
unknown <SmithaRai.com>
2022-06-09 17:09:44 +0530

    Added : added event binding technique to bind click event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 class App extends React.Component{
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super();
     this.state={
       title:'hello from Bridgelabz'
     }
+  } 
+
+  //onClick function
+  onClick = ($event) => {
+    console.log("save button is clicked!",$event);
+    window.open(this.url ,"blank");
   }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo} onClick = {this.onClick}
+         alt = "The Bridgelabz logo: a bridge to employment through lab works"/>
       </div>
     );
   }
